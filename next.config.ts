fff63e111ca242591b86bac3ef3569b8041d85e4
umut karaytu <umutkaraytu20@gmail.com>
2026-08-03 22:14:39 +0300

    f

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ const nextConfig: NextConfig = {
       "img-src 'self' data: blob:",
       "font-src 'self' data:",
       "style-src 'self' 'unsafe-inline'",
-      "script-src 'self' 'unsafe-inline'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
       "connect-src 'self' https://www.google-analytics.com",
       "frame-src https://www.google.com https://maps.google.com",
     ].join("; ");
